4ab713109e7725e18637ffd0f1fbae89b957b27a
Debashis Patra <debashispatra0992@gmail.com>
2026-02-10 21:51:30 +0530

    Animated-Submit-Button
    Animated-Submit-Button

---
+++ Animated-Submit-Button/script.js
@@ -0,0 +1,14 @@
+
+let btn = document.getElementById('btn');
+
+btn.addEventListener("click", function () {
+    btn.setAttribute('class', 'submit process');
+    btn.innerHTML = 'Processing';
+    setTimeout(() => {
+        btn.setAttribute('class', 'submit submitted');
+        btn.innerHTML = `
+     <span class="tick">&#10004;</span>
+     Submitted
+     `;
+    }, 5000);
+});
